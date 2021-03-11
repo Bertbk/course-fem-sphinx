23d85ca4cbbd2c03598bd7c40d33a6d430741e5b
Bertrand Thierry <bthierry@math.cnrs.fr>
2021-03-11 16:52:03 +0100

    colorize

--- _static/app/weak-formulation/main.js
+++ _static/app/weak-formulation/main.js
@@ -50,13 +50,13 @@ var fem_wf = new function() {
         }
     };
     // 5 small blocks EDP vs. 7 small blocks in WF
-    var RigidityMatrix = new RigidityOperator(["+0", "-Δu", "-aΔu", "+Δu", "+aΔ"], ["", "∫<sub>Ω</sub> ∇u·∇v", "a∫<sub>Ω</sub> ∇u·∇v", "-∫<sub>Ω</sub> ∇u·∇v", "-a∫<sub>Ω</sub> ∇u·∇v"], ["", "-", "-a", "+","+a"], ["", "+", "+a", "-","-a"], 5, 1);
-    var MassMatrix = new MassOperator(["+0", "+u", "+bu", "-u", "-bu"], ["", "∫<sub>Ω</sub> u v", "∫<sub>Ω</sub> u v","∫<sub>Ω</sub> u v","∫<sub>Ω</sub> u v"], ["", "+", "+b", "-","-b"], 5, 1);
-    var RHS = new PDEOperator(["0", "f"], ["0","+∫<sub>Ω</sub> f v"], 2, 1);
+    var RigidityMatrix = new RigidityOperator(["+0", "-\\Delta u", "-a\\Delta u", "+\\Delta u", "+a\\Delta "], ["", "\\int_{\\Omega} \\nabla u\\cdot\\nabla v", "a\\int_{\\Omega} \\nabla u\\cdot\\nabla v", "-\\int_{\\Omega} \\nabla u\\cdot\\nabla v", "-a\\int_{\\Omega} \\nabla u\\cdot\\nabla v"], ["", "-", "-a", "+","+a"], ["", "+", "+a", "-","-a"], 5, 1);
+    var MassMatrix = new MassOperator(["+0", "+u", "+bu", "-u", "-bu"], ["", "\\int_{\\Omega} u v", "\\int_{\\Omega} u v","\\int_{\\Omega} u v","\\int_{\\Omega} u v"], ["", "+", "+b", "-","-b"], 5, 1);
+    var RHS = new PDEOperator(["0", "f"], ["0","+\\int_{\\Omega} f v"], 2, 1);
 
-    var NeumannBorder = new PDEBC(["∂<sub>n</sub> u", "∂<sub>n</sub> u", "∂<sub>n</sub> u"], ["0", "g<sub>N</sub>"], ["",""], ["", "∫<sub>Γ<sub>N</sub></sub> g<sub>N</sub> v"], 2,0);
-    var DirichletBorder = new PDEBC(["∂<sub>n</sub> u", "u", "u"], ["0", "0",  "g<sub>D</sub>"], ["","",""], ["","",""], 3, 1);
-    var FourierBorder = new PDEBC(["∂<sub>n</sub>u", "∂<sub>n</sub>u + αu" , "∂<sub>n</sub>u + αu"], ["0", "0", "g<sub>F</sub>"],["","α∫<sub>Γ<sub>F</sub></sub> u v","α∫<sub>Γ<sub>F</sub></sub> u v"],["","","∫<sub>Γ<sub>F</sub></sub> g<sub>F</sub> v"], 3, 1);
+    var NeumannBorder = new PDEBC(["\\partial_{\\mathbf{n}}u", "\\partial_{\\mathbf{n}}u", "\\partial_{\\mathbf{n}}u"], ["0", "g_{N}"], ["",""], ["", "\\int_{\\Gamma_N} g_N v"], 2,0);
+    var DirichletBorder = new PDEBC(["\\partial_{\\mathbf{n}}u", "u", "u"], ["0", "0",  "g_D"], ["","",""], ["","",""], 3, 1);
+    var FourierBorder = new PDEBC(["\\partial_{\\mathbf{n}}u", "\\partial_{\\mathbf{n}}u + \\alpha u" , "\\partial_{\\mathbf{n}}u + \\alpha u"], ["0", "0", "g_F"],["","\\alpha \\int_{\\Gamma_F} u v","\\alpha \\int_{\\Gamma_F} u v"],["","","\\int_{\\Gamma_F} g_F v"], 3, 1);
     
     // "parser"
     var keyToVar={
@@ -80,13 +80,13 @@ var fem_wf = new function() {
         var elems = document.getElementsByClassName("PDEOperator");
         for (i = 0; i < elems.length; i++) {
             var op = keyToVar[elems[i].dataset.component];
-            elems[i].innerHTML= op.pde_txt[op.current];
+            elems[i].innerHTML= "\\(" + op.pde_txt[op.current] + "\\)";
         }
         elems = document.getElementsByClassName("PDEBC");
         for (i = 0; i < elems.length; i++) {
             var bc = keyToVar[elems[i].dataset.component];
-            elems[i].getElementsByTagName('td')[0].innerHTML= bc.pde_lhs[bc.current];
-            elems[i].getElementsByTagName('td')[2].innerHTML= bc.pde_rhs[bc.current];            
+            elems[i].getElementsByTagName('td')[0].innerHTML= "\\(" + bc.pde_lhs[bc.current] + "\\)";
+            elems[i].getElementsByTagName('td')[2].innerHTML= "\\(" + bc.pde_rhs[bc.current] + "\\)";            
         }
     };
 
@@ -102,61 +102,69 @@ var fem_wf = new function() {
             elems[i].innerHTML = "";
         }
         // What is the functionnal space ?
-        var spaceFun = "H<sup>1</sup>(Ω)";
+        var spaceFun = "H^1(\\Omega)";
         var unknown = "u";
         document.getElementById("dirichlet-extension").innerHTML="";
         if(DirichletBorder.isSet()){
-            spaceFun = "H<sup>1</sup><sub>D</sub>(Ω)"; 
+            spaceFun = "\\class{dirichlet ls-compo}{H^1_D(\\Omega)}"; 
             if(DirichletBorder.pde_rhs[DirichletBorder.current] != 0){
-            document.getElementById("dirichlet-extension").innerHTML="In addition: u := u + u<sub>g</sub> with u<sub>D</sub> an adequate extension of g<sub>D</sub>";
+            document.getElementById("dirichlet-extension").innerHTML="Avec, de plus,  \\(u := u + u_D\\) avec \\(u_D\\) un relèvement de \\(g_D\\)";
             }
         }
         // wf-equation
-        var equation = "";
-        var wf_LHS = "";
-        var wf_RHS = "";
+        var wf_LHS = ""; var wf_RHS = "";
+        var ls_LHS =""; var ls_RHS="";
         if(RHS.isSet()){
-            wf_RHS += RHS.wf_txt[RHS.current];
-            document.getElementById("ls-rhs").innerHTML = "B";
+            wf_RHS += "\\class{rhs wf-compo}{"+ RHS.wf_txt[RHS.current] +"}";
+            ls_RHS += "\\class{rhs ls-compo}{"+ "B" +"}";
         }
         if(RigidityMatrix.isSet()){
-            wf_LHS += RigidityMatrix.wf_txt[RigidityMatrix.current];
-            document.getElementById("ls-rigidity").innerHTML = RigidityMatrix.rhs_coef[RigidityMatrix.current]+"D";
+            wf_LHS += "\\class{rigidity wf-compo}{"+RigidityMatrix.wf_txt[RigidityMatrix.current]+"}";
+            ls_LHS += "\\class{rigidity ls-compo}{"+RigidityMatrix.rhs_coef[RigidityMatrix.current] + "D" +"}";
+        }
+        if(MassMatrix.isSet()){
+            wf_LHS += "\\class{mass wf-compo}{" + MassMatrix.coef[MassMatrix.current]+ MassMatrix.wf_txt[MassMatrix.current]+"}";
+            ls_LHS += "\\class{mass ls-compo}{" + MassMatrix.coef[MassMatrix.current]+"M"+"}";
+        }
+        if(RigidityMatrix.isSet()){
             // Neumann / Fourier condition can be visible
             if(NeumannBorder.wf_rhs[NeumannBorder.current])
             {
-                wf_RHS += RigidityMatrix.rhs_coef[RigidityMatrix.current]+ NeumannBorder.wf_rhs[NeumannBorder.current];
-                document.getElementById("ls-neumann-rhs").innerHTML = RigidityMatrix.rhs_coef[RigidityMatrix.current]+"M<sub>Γ<sub>N</sub></sub>g<sub>N</sub>";
+                wf_RHS += "\\class{wf-compo neumann}{" + RigidityMatrix.rhs_coef[RigidityMatrix.current]+ NeumannBorder.wf_rhs[NeumannBorder.current] +"}";
+                ls_RHS += "\\class{ls-compo neumann}{" + RigidityMatrix.rhs_coef[RigidityMatrix.current]+"M_{\\Gamma_N}g_N" +"}";
             }
             if(FourierBorder.wf_lhs[FourierBorder.current])
             { 
-                wf_LHS += RigidityMatrix.lhs_coef[RigidityMatrix.current]+ FourierBorder.wf_lhs[FourierBorder.current];
-                document.getElementById("ls-fourier-lhs").innerHTML = RigidityMatrix.lhs_coef[RigidityMatrix.current]+"αM<sub>Γ<sub>F</sub></sub>";
+                wf_LHS += "\\class{wf-compo fourier}{" + RigidityMatrix.lhs_coef[RigidityMatrix.current]+ FourierBorder.wf_lhs[FourierBorder.current] + "}";
+                ls_LHS += "\\class{ls-compo fourier}{" + RigidityMatrix.lhs_coef[RigidityMatrix.current]+"\\alpha M_{\\Gamma_F}" + "}";
             }
             if(FourierBorder.wf_rhs[FourierBorder.current])
             {
-                wf_RHS += RigidityMatrix.rhs_coef[RigidityMatrix.current]+ FourierBorder.wf_rhs[FourierBorder.current];
-                document.getElementById("ls-fourier-rhs").innerHTML = RigidityMatrix.rhs_coef[RigidityMatrix.current]+"M<sub>Γ<sub>F</sub></sub>g<sub>F</sub>";
+                wf_RHS += "\\class{wf-compo fourier}{" + RigidityMatrix.rhs_coef[RigidityMatrix.current]+ FourierBorder.wf_rhs[FourierBorder.current] + "}";
+                ls_RHS += "\\class{ls-compo fourier}{" + RigidityMatrix.rhs_coef[RigidityMatrix.current]+"M_{\\Gamma_F}g_F" + "}";
             }
         }
-        if(MassMatrix.isSet()){
-            wf_LHS += MassMatrix.coef[MassMatrix.current]+ MassMatrix.wf_txt[MassMatrix.current];
-            document.getElementById("ls-mass").innerHTML = MassMatrix.coef[MassMatrix.current]+"M";
-        }
-        equation = wf_LHS + " = " + wf_RHS;
-        // Now fill
-        document.getElementById("wf-space-u").innerHTML = spaceFun;
-        document.getElementById("wf-space-v").innerHTML = spaceFun;
-        document.getElementById("wf-unknown").innerHTML = unknown;
-        document.getElementById("wf-equation").innerHTML = equation;
+
+        // Concatenate LHS and RHS
+        var equation = wf_LHS + " = " + wf_RHS;
+        // Now fill 
+        document.getElementById("math-weak-formulation").innerHTML = "\\( \\left\\\{\\begin{aligned}& \\text{Trouver }"+ unknown + "\\in "+spaceFun +"\\text{ tel que, }\\forall v\\in "+spaceFun +"\\\\ &"+ equation+"\\end{aligned}\\right.\\)";
+
+        //Same for linear system
+        var ls_equation = "\\left(" + ls_LHS + "\\right) U = " + ls_RHS;
+        document.getElementById("math-linear-system").innerHTML = "\\(" +ls_equation +"\\)";
     };
 
     // Compute everything
     var computeAll = function(){
         computePDE();
         computeWF();
+        MathJax.typeset();
     };
-    // Run it at startup
-    computeAll();
 
+    // Fill the DOM at startup (do not launch MathJax!)
+    computePDE();
+    computeWF();
+
+    
 };
